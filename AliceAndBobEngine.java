015a565fba7805eaa031ecf038e2ca366570774f
Von Le <vle@zipcodes-MacBook-Pro.local>
2020-02-05 16:14:12 -0500

    fixed logic again. Used equals to correctly compare strings

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -16,7 +16,7 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-        if(input == "Alice" || input == "Bob"){
+        if(input.equals("Alice") || input.equals("Bob")){
           result = "Hello " + input;
         }else{
           result = "I DON'T KNOW YOU, YOU ARE SCARY";
