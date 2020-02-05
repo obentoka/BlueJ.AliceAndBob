8c9b707e29118f10d313c5c4363799fb3b878b61
Von Le <vle@zipcodes-MacBook-Pro.local>
2020-02-05 16:00:09 -0500

    fixed logic

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -8,7 +8,7 @@
 public class AliceAndBobEngine
 {
     String result;
-    
+
     AliceAndBobEngine(){
         result = "";
     }
@@ -16,7 +16,11 @@ public class AliceAndBobEngine
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
     public String evaluateAliceOrBob(String input)
     {
-    
+        if(input == "Alice" || input == "Bob"){
+          result = "Hello " + input;
+        }else{
+          result = "I DON'T KNOW YOU, YOU ARE SCARY";
+        }
         return result;
     }
 }
